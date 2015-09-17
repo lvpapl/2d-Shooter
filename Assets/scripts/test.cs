b71ce69f4f706a25fec35d02638cfafaacc0badf
lvapl <Jikol7576@gmail.com>
2015-09-17 22:54:07 +0700

    setup

---
+++ Assets/scripts/test.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+	private Vector3 target;
+	private Transform myTransform;
+
+	// Use this for initialization
+	void Start () {
+		myTransform = gameObject.transform;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition)-GameObject.FindGameObjectWithTag("Player").transform.position;
+		//myTransform.rotation = Quaternion.Slerp(myTransform.rotation,
+		                                       // Quaternion.LookRotation(target-myTransform.position),
+		                                       // Time.deltaTime * 100);
+
+	}
+}
